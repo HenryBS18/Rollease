f8d8688899905585fa27132ea8acb2d702cd68fd
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-05-27 18:08:27 +0800

    Add logout button on Sidebar

--- lib/views/widgets/sidebar.dart
+++ lib/views/widgets/sidebar.dart
@@ -13,76 +13,97 @@ class _SidebarState extends State<Sidebar> {
     return Drawer(
       child: ListView(
         children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.7388,
-                height: 132,
-                child: Row(
-                  children: [
-                    Image.asset(
-                      "assets/Logo Utama.png",
-                      width: 132,
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 8),
-              const Padding(
-                padding: EdgeInsets.only(left: 16),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      "Hi Henry",
-                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.7388,
+                      height: 132,
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            "assets/Logo Utama.png",
+                            width: 132,
+                          ),
+                        ],
+                      ),
                     ),
+                    const SizedBox(height: 8),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 16),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Hi Henry",
+                            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    const Divider(),
+                    ListTile(
+                      leading: const Icon(Icons.person),
+                      title: const Text("Profile"),
+                      onTap: () {},
+                    ),
+                    const Divider(),
+                    ListTile(
+                      leading: const Icon(Icons.payment),
+                      title: const Text("Payment"),
+                      onTap: () {},
+                    ),
+                    const Divider(),
+                    ListTile(
+                      leading: const Icon(Icons.history),
+                      title: const Text("Riding History"),
+                      onTap: () {},
+                    ),
+                    const Divider(),
+                    ListTile(
+                      leading: const Icon(Icons.confirmation_num),
+                      title: const Text("Promos"),
+                      onTap: () {},
+                    ),
+                    const Divider(),
+                    ListTile(
+                      leading: const Icon(Icons.file_copy),
+                      title: const Text("Terms & Conditions"),
+                      onTap: () {},
+                    ),
+                    const Divider(),
+                    ListTile(
+                      leading: const Icon(Icons.settings),
+                      title: const Text("Settings"),
+                      onTap: () {},
+                    ),
+                    const Divider(),
                   ],
                 ),
-              ),
-              const SizedBox(height: 16),
-              const Divider(),
-              ListTile(
-                leading: const Icon(Icons.person),
-                title: const Text("Profile"),
-                onTap: () {},
-              ),
-              const Divider(),
-              ListTile(
-                leading: const Icon(Icons.payment),
-                title: const Text("Payment"),
-                onTap: () {},
-              ),
-              const Divider(),
-              ListTile(
-                leading: const Icon(Icons.history),
-                title: const Text("Riding History"),
-                onTap: () {},
-              ),
-              const Divider(),
-              ListTile(
-                leading: const Icon(Icons.airplane_ticket),
-                title: const Text("Promos"),
-                onTap: () {},
-              ),
-              const Divider(),
-              ListTile(
-                leading: const Icon(Icons.assignment_turned_in),
-                title: const Text("Terms & Conditions"),
-                onTap: () {},
-              ),
-              const Divider(),
-              ListTile(
-                leading: const Icon(Icons.settings),
-                title: const Text("Settings"),
-                onTap: () {},
-              ),
-              const Divider(),
-            ],
-          )
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 64),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.logout,
+                      color: Color(0xffFC6464),
+                    ),
+                    title: const Text(
+                      "Logout",
+                      style: TextStyle(color: Color(0xffFC6464), fontWeight: FontWeight.bold),
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
