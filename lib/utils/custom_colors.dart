fb3085e49f90da4d7d5cf8d1729177ca88f9a65c
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-06-01 23:55:26 +0800

    Add new custom colors

--- lib/utils/custom_colors.dart
+++ lib/utils/custom_colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/cupertino.dart';
 
 class CustomColors {
   static const Color primary = Color(0xffA2C90C);
+  static const Color secondary = Color(0xffe4efb7);
+  static const Color ovo = Color(0xff8444B7);
 }
