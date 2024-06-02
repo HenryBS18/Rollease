2998efb80d2b103242d6fbb70f5abf6e65eb7c04
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-06-03 00:38:43 +0800

    Apply bloc state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rollease/bloc/mode_bloc/mode_bloc.dart';
+import 'package:rollease/bloc/photo_bloc/photo_bloc.dart';
+import 'package:rollease/bloc/selected_vehicle_bloc/selected_vehicle_bloc.dart';
 import 'package:rollease/views/pages/pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
@@ -10,32 +16,40 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Rollease",
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => ModeBloc()..add(NormalModeEvent())),
+        BlocProvider(create: (context) => PhotoBloc()),
+        BlocProvider(create: (context) => SelectedVehicleBloc()),
+      ],
+      child: MaterialApp(
+        title: "Rollease",
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/home',
+        routes: {
+          // '/': (context) => const SplashScreen(),
+          '/home': (context) => const HomePage(),
+          '/payment': (context) => const PaymentPage(),
+          '/promo': (context) => const PromoPage(),
+          '/login': (context) => const LoginPage(),
+          '/register': (context) => const RegisterPage(),
+          '/booking': (context) => const BookingPage(),
+          '/tnc': (context) => const TermsAndConditonsPage(),
+          '/onboarding': (context) => const OnBoardingPage(),
+          '/tutorial-booking': (context) => const TutorialBookingPage(),
+          '/profile': (context) => const ProfilePage(),
+          '/history': (context) => const RidingHistoryPage(),
+          '/how-to-ride': (context) => const HowToRidePage(),
+          '/scan': (context) => const ScanQrPage(),
+          '/bill': (context) => const BillInfoPage(),
+          '/photo': (context) => const PhotoPage(),
+          '/photo-result': (context) => const PhotoResultPage(),
+        },
       ),
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const SplashScreen(),
-        '/home': (context) => const HomePage(),
-        '/payment': (context) => const PaymentPage(),
-        '/promo': (context) => const PromoPage(),
-        '/login': (context) => const LoginPage(),
-        '/register': (context) => const RegisterPage(),
-        '/rent': (context) => const RentDuration(),
-        '/tnc': (context) => const TermsAndConditonsPage(),
-        '/onboarding': (context) => const OnBoardingPage(),
-        '/tutorial': (context) => const TutorialBookingPage(),
-        '/profile': (context) => const ProfilePage(),
-        '/history': (context) => const HistoryPage(),
-        '/how-to-ride': (context) => const HowToRidePage(),
-        '/scan': (context) => const ScanQrPage(),
-        '/end': (context) => const EndRidePage(),
-        '/bill': (context) => const BillInfo(),
-      },
     );
   }
 }
