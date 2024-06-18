041d985e2775600bb40d139c113dc9fa3dd022c8
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-06-18 15:49:50 +0800

    Update station and vehicle model

--- lib/models/vehicle.dart
+++ lib/models/vehicle.dart
@@ -1,9 +1,21 @@
+import 'package:latlong2/latlong.dart';
+
 class Vehicle {
   final int id;
   final String name;
   final String type;
+  final String imageUrl;
   final int battery;
   final bool inUse;
+  final LatLng currentCoordinates;
 
-  Vehicle({required this.id, required this.name, required this.type, required this.battery, required this.inUse});
+  Vehicle({
+    required this.id,
+    required this.name,
+    required this.type,
+    required this.imageUrl,
+    required this.battery,
+    required this.inUse,
+    required this.currentCoordinates,
+  });
 }
