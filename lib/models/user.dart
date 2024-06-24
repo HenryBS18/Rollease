392df1bf6753f62a49aec3db1c0fe4ee14e4295b
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2024-06-25 01:22:54 +0800

    Create user model

---
+++ lib/models/user.dart
@@ -0,0 +1,9 @@
+class User {
+  final int id;
+  final String name;
+  final String email;
+  final DateTime birthdate;
+  final String token;
+
+  User({required this.id, required this.name, required this.email, required this.birthdate, required this.token});
+}
